f0e16644852e5c12d7efbbc39093574666c2e2e6
Moritz Knoll <moritz.knoll@gmx.net>
2025-12-16 08:44:36 +0100

    Changed API keys for more safety

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -8,13 +8,21 @@ openssl rand -base64 18 > secrets/wp_db_password.txt
 # Restrict access to secrets (owner only)
 chmod 600 secrets/*.txt
 
+# Generate WordPress admin password
+openssl rand -base64 18 > secrets/wp_admin_password.txt
+
 # Create .env and .env.example files if not present
-cat > .env <<'EOF'
+cat > ./srcs/.env <<'EOF'
 WORDPRESS_DB_HOST=mariadb
 WORDPRESS_DB_USER=wp_user
 WORDPRESS_DB_NAME=wordpress
 MYSQL_DATABASE=wordpress
 MYSQL_USER=wp_user
+WP_URL=localhost
+WP_TITLE=inception
+WP_ADMIN_USER=admin
+WP_ADMIN_PASSWORD_FILE=/run/secrets/wp_admin_password
+WP_ADMIN_EMAIL=admin@inception.local
 EOF
 
 # Add secrets and .env to .gitignore
